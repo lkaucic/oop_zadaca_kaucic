5dfe1debe9577eee8e7744d69cab306ea4d87bd8
lkaucic <73308606+lkaucic@users.noreply.github.com>
2020-11-17 16:27:29 +0100

    Add files via upload

---
+++ DZ2/MyLibrary/Episode.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace MyLibrary
+{
+    public class Episode
+    {
+
+        List<double> scores = new List<double>();  //a list which will keep track of all generated ratings
+        Description description = new Description();
+
+        public int AudienceCount { get; private set; }
+        public double SumOfRatings { get; private set; }
+        public double MaxRating { get; private set; }
+
+        public Episode() { }
+        public Episode(int audienceCount, double sumOfRatings, double maxRating, Description about) { AudienceCount = audienceCount; SumOfRatings = sumOfRatings; MaxRating = maxRating; description = about; }
+
+
+
+        public void AddView(double rating)  // when adding a viewer, Audience count is increased by 1, we're adding generated rating to our list and also adding that rating to sum of all ratings
+        {
+
+            AudienceCount += 1;
+            scores.Add(rating);
+            SumOfRatings += rating;
+        }
+
+        public double GetMaxScore()  //with foreach loop we're going through the list and searching for the max value
+        {
+            double max = 0;
+            foreach (double score in scores)
+            {
+                if (score > max)
+                {
+                    max = score;
+                }
+            }
+            return max;
+        }
+
+        public double GetAverageScore() 
+        {
+           
+            return SumOfRatings/AudienceCount;
+        }
+
+        public int GetViewerCount()
+        {
+            return AudienceCount;
+        }
+
+        public override string ToString()
+        {
+            return AudienceCount + ", " + SumOfRatings + ", " + MaxRating + ", " + description.GetNumberOfEpisode() + ", " + description.GetDurationOfEpisode() + ", " + description.GetNameOfEpisode();
+        }
+
+        public static bool operator <(Episode lhs, Episode rhs)
+        {
+            if (lhs is null) return rhs is null;
+            return lhs.GetAverageScore() < rhs.GetAverageScore();
+        }
+
+        public static bool operator >(Episode lhs, Episode rhs)
+        {
+            return !(lhs.GetAverageScore() < rhs.GetAverageScore());
+        }
+    }
+
+}
---
+++ DZ2/MyLibrary/TvUtilities.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary
+{
+    public class TvUtilities
+    {
+        public static double Generate()
+        {
+
+            Random generator = new Random();
+            double rating = (generator.NextDouble()) * 10; //since generator.NextDouble() will return a double value in range from 0.0 to 1.0, we have to multyply it by 10 to get desirable rating in rango from 0.0 to 10.0
+            return rating;
+        }
+
+        public static void Sort(Episode[] episodes)
+        {
+            Episode temp;
+
+            for (int i = episodes.Length - 1; i >= 0; i--)
+            {
+                for (int j = episodes.Length-1; j > episodes.Length - 1 - i; j--)
+                {
+                    if (episodes[j] > episodes[j - 1])
+                    {
+                        temp = episodes[j];
+                        episodes[j] = episodes[j - 1];
+                        episodes[j - 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public static Episode Parse(string episodeFlow)
+        {
+            
+            string[] episodeString = episodeFlow.Split(',');
+            int AudienceCount = int.Parse(episodeString[0]);
+            double SumOfRatings = double.Parse(episodeString[1])/100;
+            double MaxRating = double.Parse(episodeString[2])/100;
+            int numberOfEpisode = int.Parse(episodeString[3]);
+            TimeSpan durationOfEpisode = TimeSpan.Parse(episodeString[4]);
+            string nameOfEpisode = episodeString[5];
+
+            Description description = new Description(numberOfEpisode, durationOfEpisode, nameOfEpisode);
+            return new Episode(AudienceCount, SumOfRatings, MaxRating, description);
+            
+        }
+           
+
+    }
+}
