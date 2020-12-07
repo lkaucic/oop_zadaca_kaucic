505646b6edce826f9cf8a8527f50258359783f67
lkaucic <73308606+lkaucic@users.noreply.github.com>
2020-12-07 21:51:19 +0100

    Add files via upload

---
+++ DZ3/MyLibrary/Description.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary
+{
+    public class Description
+    {
+        int numberOfEpisode;
+        TimeSpan durationOfEpisode;
+        string nameOfEpisode;
+
+        public Description() { }
+
+        public Description(int number, TimeSpan duration, string name)
+        {
+            numberOfEpisode = number;
+            durationOfEpisode = duration;
+            nameOfEpisode = name;
+        }
+
+        public override String ToString()
+        {
+            return numberOfEpisode + "," + durationOfEpisode + "," + nameOfEpisode;
+        }
+        public int GetNumberOfEpisode()
+        {
+            return numberOfEpisode;
+        }
+        public TimeSpan GetDurationOfEpisode()
+        {
+            return durationOfEpisode;
+        }
+        public string GetNameOfEpisode()
+        {
+            return nameOfEpisode;
+        }
+
+
+    }
+}
---
+++ DZ3/MyLibrary/Season.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary
+{
+    public class Season
+    {
+        int noOfSeason;
+        Episode[] episodes;
+        
+        public int Length{ get; }
+        
+
+        public Episode this[int i]
+        {
+            get { return episodes[i]; }
+            set { episodes[i] = value; }
+        }
+
+        public Season(int noOfSeason, Episode[] episodes)
+        {
+            this.noOfSeason = noOfSeason;
+            this.episodes = episodes;
+            this.Length = episodes.Length;
+        }
+
+        public void AddView(double randomRating)
+            //adding view to each episode in season
+        {
+            for(int i = 0; i<episodes.Length; i++)
+            {
+                episodes[i].AddView(randomRating);
+            }
+        }
+
+        public override string ToString()
+        {
+            int audienceCount = 0;
+            TimeSpan totalDuration = new TimeSpan(0,0,0);
+          
+
+            string returnSeason = null;
+
+
+            for(int i = 0; i<episodes.Length; i++)
+            {
+                audienceCount += episodes[i].AudienceCount;
+                totalDuration = totalDuration.Add(episodes[i].GetDescription().GetDurationOfEpisode());
+            }
+            returnSeason += $"Season{noOfSeason}:\n=================================================\n";
+
+            for (int i = 0; i<episodes.Length; i++)
+            {
+                returnSeason += episodes[i].ToString();
+                returnSeason += "\n";
+            }
+
+            returnSeason += "Report:\n";
+            returnSeason += "=================================================\n";
+            returnSeason += $"Total viewers: {audienceCount}\nTotal duration: {totalDuration}\n";
+            returnSeason += "=================================================\n";
+
+            return returnSeason;
+
+        }
+
+    }
+}
